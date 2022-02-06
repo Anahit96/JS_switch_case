da2bc7197d231fa2d28741b25561b0cc380d2990
Anahit96 <anahit.aleksanyan.96.aa@gamil.com>
2022-02-06 20:24:06 +0400

    implement script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let isOpen = true
+switch (isOpen) {
+    case true:
+        alert('Close');
+        break;
+    case false:
+        alert('Open');
+        break;
+    default:
+        alert('Undefined');
+}
